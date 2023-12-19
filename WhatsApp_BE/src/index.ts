b807e00bbbfb1398aca1c22ea004798e49715ca8
jardila <jardila@bancolombia.com.co>
2023-12-19 08:57:48 -0500

    first commit

---
+++ WhatsApp_BE/src/index.ts
@@ -0,0 +1,13 @@
+// src/index.js
+
+import dotenv from "dotenv";
+import app from "./app";
+
+dotenv.config();
+
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
